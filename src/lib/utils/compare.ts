f9c20b6d8e97c6ba5579b1ea328455a82230c918
web-padawan <iamkulykov@gmail.com>
2020-08-30 15:43:42 +0300

    refactor: improve types from upstream

--- src/lib/utils/compare.ts
+++ src/lib/utils/compare.ts
@@ -5,7 +5,16 @@ export const equalColorObjects = (first: HSL | HSV | RGB, second: HSL | HSV | RG
   if (first === second) return true;
 
   for (const prop in first) {
-    if (first[prop] !== second[prop]) return false;
+    // The following allows for a type-safe calling of this function (first & second have to be HSL, HSV, or RGB)
+    // with type-unsafe iterating over object keys. TS does not allow this without an index (`[key: string]: number`)
+    // on an object to define how iteration is normally done. To ensure extra keys are not allowed on our types,
+    // we must cast our object to unknown (as RGB demands `r` be a key, while `Record<string, x>` does not care if
+    // there is or not), and then as a type TS can iterate over.
+    if (
+      ((first as unknown) as Record<string, number>)[prop] !==
+      ((second as unknown) as Record<string, number>)[prop]
+    )
+      return false;
   }
 
   return true;
