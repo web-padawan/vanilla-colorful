2fd2778c40eb271741545d5d3ff2536ff585646f
web-padawan <iamkulykov@gmail.com>
2020-09-14 21:13:33 +0300

    test: update visual test screenshots

--- src/test/visual/visual.test.ts
+++ src/test/visual/visual.test.ts
@@ -3,14 +3,31 @@ import { fixture } from '@open-wc/testing-helpers';
 import '../../hex-color-picker.js';
 import '../../hsl-color-picker.js';
 import '../../hsl-string-color-picker.js';
+import '../../hsla-color-picker.js';
+import '../../hsla-string-color-picker.js';
 import '../../hsv-color-picker.js';
+import '../../hsva-color-picker.js';
 import '../../rgb-color-picker.js';
 import '../../rgb-string-color-picker.js';
+import '../../rgba-color-picker.js';
+import '../../rgba-string-color-picker.js';
 
 describe('visual tests', () => {
   let picker: HTMLElement;
 
-  ['hex', 'hsl', 'hsl-string', 'hsv', 'rgb', 'rgb-string'].forEach((type) => {
+  [
+    'hex',
+    'hsl',
+    'hsl-string',
+    'hsla',
+    'hsla-string',
+    'hsv',
+    'hsva',
+    'rgb',
+    'rgb-string',
+    'rgba',
+    'rgba-string'
+  ].forEach((type) => {
     describe(`${type}-color-picker`, () => {
       beforeEach(async () => {
         picker = await fixture(`
