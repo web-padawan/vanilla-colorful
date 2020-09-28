3cb56509b7dc404033f8a5f9686caba24050ce88
web-padawan <iamkulykov@gmail.com>
2020-09-28 14:43:28 +0300

    test: update tests to use alpha slider

--- src/test/color-picker.test.ts
+++ src/test/color-picker.test.ts
@@ -1,10 +1,13 @@
 import { expect } from '@esm-bundle/chai';
 import sinon from 'sinon';
 import { fixture, html, nextFrame } from '@open-wc/testing-helpers';
-import { hexToHsva, hsvaToRgbString } from '../lib/utils/convert';
+import { hsvaToRgbString, rgbaToHsva } from '../lib/utils/convert';
 import type { HexColorPicker } from '../hex-color-picker';
 import type { Hue } from '../lib/components/hue';
+import type { Alpha } from '../lib/components/alpha';
 import type { Saturation } from '../lib/components/saturation';
+import type { RgbaColorPicker } from '../rgba-color-picker';
+import '../rgba-color-picker.js';
 
 class FakeMouseEvent extends MouseEvent {
   constructor(type: string, values: { pageX: number; pageY: number }) {
@@ -39,11 +42,11 @@ const middleOfNode = (node: Element) => {
   return { y: bcr.top + bcr.height / 2, x: bcr.left + bcr.width / 2 };
 };
 
-const getInteractive = (node: Hue | Saturation) => {
+const getInteractive = (node: Hue | Saturation | Alpha) => {
   return node.shadowRoot!.getElementById('interactive') as HTMLElement;
 };
 
-const getPointer = (node: Hue | Saturation) => {
+const getPointer = (node: Hue | Saturation | Alpha) => {
   return node.shadowRoot!.querySelector('[part=pointer]') as HTMLElement;
 };
 
@@ -149,18 +152,22 @@ describe('hex-color-picker', () => {
     });
   });
 
-  describe('hue and saturation', () => {
+  describe('hue, saturation, alpha', () => {
+    let picker: RgbaColorPicker;
+
     let hue: Hue;
     let saturation: Saturation;
+    let alpha: Alpha;
 
     beforeEach(async () => {
-      picker = document.createElement('hex-color-picker');
-      picker.color = '#488';
+      picker = document.createElement('rgba-color-picker');
+      picker.setAttribute('color', JSON.stringify({ r: 68, b: 136, g: 136, a: 1 }));
       await nextFrame();
       document.body.appendChild(picker);
       const root = picker.shadowRoot as ShadowRoot;
       hue = root.querySelector('[part="hue"]') as Hue;
       saturation = root.querySelector('[part="saturation"]') as Saturation;
+      alpha = root.querySelector('[part="alpha"]') as Alpha;
     });
 
     afterEach(() => {
@@ -169,32 +176,36 @@ describe('hex-color-picker', () => {
 
     describe('pointers', () => {
       it('should set saturation background color', () => {
-        const hsva = hexToHsva(picker.color);
+        const hsva = rgbaToHsva(picker.color);
         const bgColor = hsvaToRgbString({ h: hsva.h, s: 100, v: 100, a: 1 });
         expect(getComputedStyle(saturation).backgroundColor).to.equal(bgColor);
       });
 
       it('should set saturation pointer color', () => {
-        const bgColor = hsvaToRgbString(hexToHsva(picker.color));
+        const bgColor = hsvaToRgbString(rgbaToHsva(picker.color));
         expect(getComputedStyle(getPointer(saturation)).color).to.equal(bgColor);
       });
 
       it('should set saturation pointer coordinates', () => {
-        const hsva = hexToHsva(picker.color);
+        const hsva = rgbaToHsva(picker.color);
         const pointer = getPointer(saturation);
         expect(pointer.style.top).to.equal(`${100 - hsva.v}%`);
         expect(pointer.style.left).to.equal(`${hsva.s}%`);
       });
 
       it('should set hue pointer color', () => {
-        const hsva = hexToHsva(picker.color);
+        const hsva = rgbaToHsva(picker.color);
         const bgColor = hsvaToRgbString({ h: hsva.h, s: 100, v: 100, a: 1 });
         expect(getComputedStyle(getPointer(hue)).color).to.equal(bgColor);
       });
 
       it('should set hue pointer coordinate', () => {
-        const hsv = hexToHsva(picker.color);
+        const hsv = rgbaToHsva(picker.color);
         expect(getPointer(hue).style.left).to.equal(`${(hsv.h / 360) * 100}%`);
+      });
+
+      it('should set alpha pointer coordinate', () => {
+        expect(getPointer(alpha).style.left).to.equal('100%');
       });
     });
 
@@ -241,8 +252,18 @@ describe('hex-color-picker', () => {
         expect(spy.callCount).to.equal(2);
       });
 
+      it('should dispatch color-changed event on alpha interaction', () => {
+        const elem = getInteractive(alpha);
+        const spy = sinon.spy();
+        picker.addEventListener('color-changed', spy);
+        const { x, y } = middleOfNode(elem);
+        elem.dispatchEvent(new FakeTouchEvent('touchstart', [{ pageX: x, pageY: y }]));
+        elem.dispatchEvent(new FakeTouchEvent('touchend', [{ pageX: x, pageY: y }]));
+        expect(spy.callCount).to.equal(1);
+      });
+
       it('should not dispatch event when hue changes for black', () => {
-        picker.color = '#000';
+        picker.color = { r: 0, g: 0, b: 0, a: 1 };
         const elem = getInteractive(hue);
         const spy = sinon.spy();
         picker.addEventListener('color-changed', spy);
@@ -255,7 +276,7 @@ describe('hex-color-picker', () => {
 
       it('should not react on mouse events after a touch interaction', () => {
         const elem = getInteractive(hue);
-        picker.color = '#00f';
+        picker.color = { r: 0, g: 0, b: 255, a: 1 };
         const spy = sinon.spy();
         picker.addEventListener('color-changed', spy);
         const { left, top, height } = elem.getBoundingClientRect();
