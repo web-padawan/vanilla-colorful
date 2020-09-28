23805f5789468dc4682d042ec53c4c37144ca831
web-padawan <iamkulykov@gmail.com>
2020-09-28 15:41:47 +0300

    feat: implement accessibility support

--- src/lib/components/hue.ts
+++ src/lib/components/hue.ts
@@ -1,6 +1,7 @@
 import { Interactive, Interaction } from './interactive.js';
 import { hsvaToHslString } from '../utils/convert.js';
 import { createTemplate, createRoot } from '../utils/dom.js';
+import { clamp } from '../utils/clamp.js';
 import styles from '../styles/hue.js';
 
 const template = createTemplate(`<style>${styles}</style>`);
@@ -9,6 +10,9 @@ export class Hue extends Interactive {
   constructor() {
     super();
     createRoot(this, template);
+    this.setAttribute('aria-label', 'Hue');
+    this.setAttribute('aria-valuemin', '0');
+    this.setAttribute('aria-valuemax', '360');
   }
 
   connectedCallback(): void {
@@ -19,15 +23,28 @@ export class Hue extends Interactive {
     }
   }
 
+  private _h!: number;
+
+  get arrowsOnly(): boolean {
+    return false;
+  }
+
+  get hue(): number {
+    return this._h;
+  }
+
   set hue(h: number) {
+    this._h = h;
     this.setStyles({
       left: `${(h / 360) * 100}%`,
       color: hsvaToHslString({ h, s: 100, v: 100, a: 1 })
     });
+    this.setAttribute('aria-valuenow', `${Math.round(h)}`);
   }
 
-  getMove(interaction: Interaction): Record<string, number> {
-    return { h: 360 * interaction.left };
+  getMove(interaction: Interaction, key?: boolean): Record<string, number> {
+    // Hue measured in degrees of the color circle ranging from 0 to 360
+    return { h: key ? clamp(this.hue + interaction.left * 360, 0, 360) : 360 * interaction.left };
   }
 }
 
