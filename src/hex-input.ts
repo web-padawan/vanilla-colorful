62cb9c21f3351cb70be965840e9e8d88d651c3dd
Toby Zerner <toby.zerner@gmail.com>
2022-09-21 10:02:13 +0930

    feat: add support for alpha to hex-input

--- src/hex-input.ts
+++ src/hex-input.ts
@@ -7,6 +7,8 @@ import { HexInputBase } from './lib/entrypoints/hex-input.js';
  *
  * @prop {string} color - Color in HEX format.
  * @attr {string} color - Selected color in HEX format.
+ * @prop {boolean} alpha - When true, `#rgba` and `#rrggbbaa` color formats are allowed.
+ * @attr {boolean} alpha - Allows `#rgba` and `#rrggbbaa` color formats.
  *
  * @fires color-changed - Event fired when color is changed.
  *
