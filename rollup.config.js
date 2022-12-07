d2afae12bd0324f66a0e3078525915007f91e820
Serhii Kulykov <iamkulykov@gmail.com>
2022-12-07 14:19:12 +0200

    chore: change Rollup config to ESM, fix scripts (#98)

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
+
+export default {
+  input: './index.html',
+  output: {
+    dir: './dist'
+  },
+  plugins: [
+    html(),
+    nodeResolve(),
+    terser()
+  ]
+};
