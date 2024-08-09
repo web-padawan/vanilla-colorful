0ac86a3418c7a66848051e98443bdf4cc666cd50
web-padawan <iamkulykov@gmail.com>
2024-08-09 14:45:03 +0300

    chore: update tests to use ES module chai version

--- src/test/color-picker.test.ts
+++ src/test/color-picker.test.ts
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from 'chai';
 import sinon from 'sinon';
 import { fixture, html, nextFrame } from '@open-wc/testing-helpers';
 import { hsvaToRgbString, rgbaToHsva } from '../lib/utils/convert';
